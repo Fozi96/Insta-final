2769ede2ceb4541d3b55cace1e245a05374a9a9e
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-10-22 03:36:14 +0000

    created post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  mount_uploader :profile_image, ImageUploader
+
+  validates :name, presence: true, length: { maximum: 30 }
+
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, on: :create
+  before_validation { email.downcase! }
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+
+  validates :profile_content, length: { maximum: 120 }
+
+  has_many :feeds
+
+  has_many :favorites, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_030438) do
+ActiveRecord::Schema.define(version: 2020_10_22_033024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.text "profile_image"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
