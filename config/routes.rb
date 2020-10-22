8d91b5ef831172ed67d73ea92140c8650b490593
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-10-22 06:05:17 +0000

    installing gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :favorites, only: [:index, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root to: 'users#new'
+  resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
+
+  resources :users do
+    collection do
+      post :confirm
+    end
+  end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
 end
