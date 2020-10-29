bdb31801c9753d6d3b8fe71b29717460b5a4f1e5
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-10-29 03:49:51 +0000

    fixed the time zone to Tokyo and fixed migrations

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,19 @@ Bundler.require(*Rails.groups)
 
 module InstagramClone
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+      config.time_zone = 'Tokyo'
+      config.active_record.default_timezone = :local
+      config.i18n.default_locale = :ja
+      # Initialize configuration defaults for originally generated Rails version.
+      config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+      # Settings in config/environments/* take precedence over those specified here.
+      config.generators do |g|
+        g.assets false
+        g.helper false
+      end
+      # Application configuration can go into files in config/initializers
+      # -- all .rb files in that directory are automatically loaded after loading
+      # the framework and any gems in your application.
+    end
   end
-end
--- db/migrate/20201028100014_add_user_ref_to_post.rb
+++ db/migrate/20201028100014_add_user_ref_to_post.rb
@@ -1,4 +1,4 @@
-class AddUserRefToPosts < ActiveRecord::Migration[5.2]
+class AddUserRefToPost < ActiveRecord::Migration[5.2]
   def change
     add_reference :posts, :user, foreign_key: true
   end
