d3cf14606d90ff297c2a97adf2f7a03ae99081c1
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-10-29 13:12:49 +0000

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_025759) do
+ActiveRecord::Schema.define(version: 2020_10_29_085740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "posts"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.text "image"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "profile_image"
@@ -26,21 +42,5 @@ ActiveRecord::Schema.define(version: 2020_10_29_025759) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-
-  create_table "favorites", force: :cascade do |t|
-      t.integer "user_id"
-      t.integer "post_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-    end
-
-    create_table "posts", force: :cascade do |t|
-      t.text "posts"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.bigint "user_id"
-      t.text "image"
-      t.index ["user_id"], name: "index_posts_on_user_id"
-    end
-    add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users"
 end
