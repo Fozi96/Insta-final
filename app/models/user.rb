63b2e62dbefacdb780fe5fc0ce55ae256eff177a
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-11-04 04:55:42 +0000

    fixed sign in function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
                     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   before_validation {email.downcase!}
   has_secure_password
-  mount_uploader :icon, ImageUploader
+  
   validates :password, presence: true, length: {minimum: 6}
   has_many :posts
   has_many :favorites, dependent: :destroy
   has_many :favorite_posts, through: :favorites, source: :post
-  validates :icon, presence: true
+
 end
